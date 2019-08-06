030e9a7023059e3ad96d86d5598435e19076d647
Jorge Barata <jbarata@MacBook.local>
2019-08-06 01:56:51 -0300

    [Githuber-01] - Tela Welcome finalizada com os estilos.

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,14 +1,40 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+  SafeAreaView
+} from "react-native";
 import styles from "./styles";
 
 // import { Container } from './styles';
 
 const Welcome = () => (
-  <View style={styles.container}>
-    <Text>Testando</Text>
-  </View>
+  <SafeAreaView style={styles.container}>
+    <StatusBar barStyle="light-content" backgroundColor="transparent" />
+    <View>
+      <Text style={styles.title}>Bem-Vindo</Text>
+      <Text style={styles.text}>
+        Para continuar, precisamos que você informe seu usuário do Github
+      </Text>
+    </View>
+
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="transparent"
+      />
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
+  </SafeAreaView>
 );
 
 export default Welcome;
