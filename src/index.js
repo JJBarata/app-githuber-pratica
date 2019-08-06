8dfdfedb4ce4de261080152cff2462d099c7cd74
Jorge Barata <jbarata@MacBook.local>
2019-08-06 00:44:36 -0300

    Primeiro Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "../ReactotronConfig";
+import React from "react";
+
+import Routes from "./routes";
+
+// import { Container } from './styles';
+
+const App = () => <Routes />;
+
+export default App;
---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+// import { Container } from './styles';
+
+const Welcome = () => (
+  <View style={styles.container}>
+    <Text>Testando</Text>
+  </View>
+);
+
+export default Welcome;
